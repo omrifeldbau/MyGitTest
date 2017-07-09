f94185a10a2f0087057b00750cd0e49abe9eb47b
omrif <omrif@WIN10-OMRIF-PC>
2017-07-09 13:54:13 +0300

    my comment

---
+++ MyGitTest123/MyGitTest123/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyGitTest123.MvcApplication" Language="C#" %>
---
+++ MyGitTest123/MyGitTest123/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyGitTest123.Startup))]
+namespace MyGitTest123
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
